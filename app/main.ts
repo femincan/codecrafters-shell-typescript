e127f65ec7d6856dc9a29273877c468b6cc8818f
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-01-13 18:55:58 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,26 +1,17 @@
-import { createInterface, type Interface } from 'node:readline/promises';
+import { createInterface } from 'node:readline/promises';
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const commandsObj = {
-  exit: exitShell,
-};
-
 while (true) {
   const answer = await rl.question('$ ');
 
-  if (Object.hasOwn(commandsObj, answer)) {
-    const commandFunction = commandsObj[answer as keyof typeof commandsObj];
-    commandFunction();
-    continue;
+  if (answer === 'exit') {
+    rl.close();
+    break;
   }
 
   console.log(`${answer}: command not found`);
-}
-
-function exitShell() {
-  rl.close();
 }
