732d99bb55dfe65c7f9e53d03ab0ce38a98f0bff
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-01-26 15:10:43 +0300

    fix: fix builtin echo command returns instead of logging to console

--- src/commands/echo.ts
+++ src/commands/echo.ts
@@ -2,5 +2,5 @@ import { createCommand } from '@/lib/command';
 import { processQuotes } from '@/lib/quote';
 
 createCommand('echo', (rest) => {
-  return processQuotes(rest);
+  console.log(processQuotes(rest));
 });
