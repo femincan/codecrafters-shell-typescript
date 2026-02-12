f7285c2c5c6a4a09fe71053192a4956587def854
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-02-12 22:43:54 +0300

    feat: add type for the completer property of readline

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,9 @@
 import 'readline/promises';
+import type { Completer } from 'readline/promises';
 
 declare module 'readline' {
   interface Interface {
     history: string[];
+    completer: Completer;
   }
 }
