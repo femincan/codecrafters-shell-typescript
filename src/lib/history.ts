8ac43fb60c29b02ae64927519196f10fd4f6a235
Furkan Emin Can <78358128+femincan@users.noreply.github.com>
2026-02-15 09:29:39 +0300

    refactor: remove unnecessary text encoder
    - Because the ReadableStream already encodes the string

--- src/lib/history.ts
+++ src/lib/history.ts
@@ -9,16 +9,13 @@ export function createFormattedHistoryStream(
   history: HistoryArray[],
   limit: number,
 ): StdStream {
-  const encoder = new TextEncoder();
   const count = Math.min(history.length, limit);
 
   return new ReadableStream({
     type: 'direct',
     pull(controller) {
       for (let i = count - 1; i >= 0; i--) {
-        controller.write(
-          encoder.encode(`${history.length - i} ${history[i]}\n`),
-        );
+        controller.write(`${history.length - i} ${history[i]}\n`);
       }
 
       controller.end();
